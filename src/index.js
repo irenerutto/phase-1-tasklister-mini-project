8a5671b5d55cce137adcb04fb1a4991277b8af80
IRENE <irenerutto@gmail.com>
2026-05-02 09:40:49 +0300

    tasklister lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //locate form 
+const form = document.getElementById("create-task-form");
+// add event listener for submit
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const task = document.getElementById("new-task-description").value;
+
+  buildToDo(task);
+});
+
+function buildToDo(task) {
+  const taskList = document.getElementById("tasks");
+// new list item
+  const li = document.createElement("li");
+
+  li.textContent = task;
+// add to we page
+  taskList.appendChild(li);
+}
 });
